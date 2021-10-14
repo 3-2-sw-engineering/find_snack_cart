9494f8350dc350fcb0a0083f87afbe4590fb2301
qf9ar8nv <ckguscjf0@gmail.com>
2021-10-14 14:38:35 +0900

    Add user API
    * user_id를 통해 원하는 user의 정보를 돌려주는 API 작성.
    * user_pw를 변경하는 API 작성 ( 안전한 비밀번호 유지를 위한 라이브러리는 추후 도입 예정 )

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,13 +18,22 @@ var User = new Schema({
 		type: String,
 		required: true,
 		trim: true,
+	},
+	user_email: {
+		type: String,
+		require: true,
+		trim: true,
+		unique: true,
+	},
+	favorite: {
+		type: [String],
 	}
 });
 
 User.statics.create = async function (user_id, user_pw, user_name) {
 	const find_user = await this.findOne({ "user_id": user_id });
 	if (find_user) {
-		throw 'user exists';
+		throw 'already user exists';
 	}
 
 	const user = new this({
@@ -44,7 +53,38 @@ User.statics.delete = async function (user_id) {
 	if (user) {
 		return this.findOneAndDelete({ "user_id": user_id });
 	} else {
-		throw 'user not exist';
+		throw 'not exist user';
+	}
+}
+
+User.statics.findUserById = async function (user_id) {
+	const user = await this.findOne({ "user_id": user_id });
+
+	if (user) {
+		return user;
+	} else {
+		throw "not exist user";
+	}
+}
+
+User.statics.changePw = async function (user_id, current_pw, change_pw) {
+	const user = await this.findOne({ "user_id": user_id });
+
+	if (user) {
+		// 비밀번호 관리 라이브러리 필요. bcrypt로 추후 변경 예정.
+		if (user.user_pw !== current_pw) { throw "wrong password" };
+
+		this.findOneAndUpdate({ "user_id": user_id }, {
+			$set: {
+				user_pw: change_pw
+			}
+		}, { new: true, useFindAndModify: false }, (err, doc) => {
+			if (err) {
+				throw err;
+			}
+		})
+	} else {
+		throw "not exist user";
 	}
 }
 
