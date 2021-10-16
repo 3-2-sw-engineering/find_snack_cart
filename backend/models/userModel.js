fd13822757df37efc51eb84acd5ef2a91b2c117d
qf9ar8nv <ckguscjf0@gmail.com>
2021-10-16 23:51:30 +0900

    Add bcrypt library
    * 비밀번호 암호화를 위한 bcrypt 라이브러리 설치.
    * 유저 생성 시 비밀번호를 암호화해서 저장하도록 변경.
    * 비밀번호 변경 로직은 수정해야함.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 var User = new Schema({
 	user_id: {
@@ -27,21 +28,46 @@ var User = new Schema({
 	},
 	favorite: {
 		type: [String],
+	},
+	role: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+	salt: {
+		type: Number,
+		required: true,
 	}
 });
 
-User.statics.create = async function (user_id, user_pw, user_name, user_email) {
+User.pre("save", function(next){
+	var user = this;
+	bcrypt.genSalt(user.salt, (err, salt) => {
+		if(err) throw err;
+		bcrypt.hash(user.user_pw, salt, (err, hash) => {
+			if(err) throw err;
+			user.user_pw = hash;
+			next();
+		});
+	});
+})
+
+User.statics.create = async function (user_id, user_pw, user_name, user_email, role) {
 	const find_user = await this.findOne({ "user_id": user_id });
 	if (find_user) {
 		throw 'already user exists';
 	}
+
+	const saltRound = Math.floor(Math.random() * 10) + 1;
 
 	const user = new this({
 		user_id: user_id,
 		user_pw: user_pw,
 		user_name: user_name,
 		user_email: user_email,
-		favorite: []
+		favorite: [],
+		role: role,
+		salt: saltRound
 	});
 
 	console.log('user 생성: ' + user_id);
@@ -73,8 +99,8 @@ User.statics.changePw = async function (user_id, current_pw, change_pw) {
 	const user = await this.findOne({ "user_id": user_id });
 
 	if (user) {
-		// 비밀번호 관리 라이브러리 필요. bcrypt로 추후 변경 예정.
-		if (user.user_pw !== current_pw) { throw "wrong password" };
+		// 비밀번호 확인
+		const result = await bcrypt.compare(current_pw, user.user_pw);
 
 		this.findOneAndUpdate({ "user_id": user_id }, {
 			$set: {
