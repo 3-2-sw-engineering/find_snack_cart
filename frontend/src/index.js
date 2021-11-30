0c288d044ed0cdffdc235d454d0c0ff6a507fab2
Jeonghyeon Kim <powergee101@gmail.com>
2021-12-01 07:08:34 +0900

    Add async functionf to use backend from frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Main } from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/*' element={(<Main/>)} />
-            </Routes>
-        </BrowserRouter>
+        <CookiesProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/*' element={(<Main/>)} />
+                </Routes>
+            </BrowserRouter>
+        </CookiesProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
