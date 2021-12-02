cf7f6011347199ccb6e24780626f1f8a2bb277a2
qf9ar8nv <ckguscjf0@gmail.com>
2021-12-02 23:43:14 +0900

    create Search Bar
    * manage, main, report page에 각각 필요한 부분에 search bar 컴포넌트 작성.
    * 엔터 또는 검색 이미지 누르면 conosole.log로 입력값을 출력하도록 구현.
    * 위도, 경도 찾는 법은 구현하지 못함.

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -5,6 +5,8 @@ import LoginIcon from '@mui/icons-material/Login';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import SearchBar from 'material-ui-search-bar';
+import SearchIcon from '@mui/icons-material/Search';
 import { Map } from "react-kakao-maps-sdk";
 import { Box } from '@mui/system';
 import { useState } from 'react';
@@ -16,110 +18,124 @@ import Report from "./Report"
 import Manage from "./Manage"
 
 function Main(props) {
-    const categories = [
-        "전체", "붕어빵/잉어빵", "타코야끼", "풀빵", "호떡", "군고구마", "꼬치", "분식", "기타"
-    ];
+	const categories = [
+		"전체", "붕어빵/잉어빵", "타코야끼", "풀빵", "호떡", "군고구마", "꼬치", "분식", "기타"
+	];
 
-    const navigate = useNavigate();
-    const [headerText, setHeaderText] = useState("군것질");
-    const [menuOpen, setMenuOpen] = useState(false);
+	const navigate = useNavigate();
+	const [headerText, setHeaderText] = useState("군것질");
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [searchText, setsearchText] = useState("")
 
-    function viewLogin() {
-        navigate("login");
-    }
+	function viewLogin() {
+		navigate("login");
+	}
 
-    function viewFavorite() {
-        navigate("favorite");
-    }
+	function viewFavorite() {
+		navigate("favorite");
+	}
 
-    function viewReport() {
-        navigate("report");
-    }
+	function viewReport() {
+		navigate("report");
+	}
 
-    function viewManage() {
-        navigate("manage");
-    }
+	function viewManage() {
+		navigate("manage");
+	}
 
-    return (
-        <div className="main-layout">
-            <AppBar position="static" color="transparent" elevation={0}>
-                <Toolbar>
-                    <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}
-                        onClick={() => setMenuOpen(true)}>
-                        <MenuIcon></MenuIcon>
-                    </IconButton>
+	function searchLocation() {
+		if (searchText === "") { return }
+		console.log(searchText)
+		setsearchText("")
+	}
 
-                    <Typography className="main-header-text" variant="h5" component="div">
-                        {`${headerText} 사장님, 어디 계세요?!`}
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+	return (
+		<div className="main-layout">
+			<AppBar position="static" color="transparent" elevation={0}>
+				<Toolbar>
+					<IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}
+						onClick={() => setMenuOpen(true)}>
+						<MenuIcon></MenuIcon>
+					</IconButton>
 
-            <Drawer open={menuOpen} onClose={() => setMenuOpen(false)}>
-                <Box sx={500} role="presentation">
-                    <List>
-                        <ListItem button>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <LoginIcon></LoginIcon>
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText onClick={viewLogin} primary="로그인"></ListItemText>
-                        </ListItem>
+					<Typography className="main-header-text" variant="h5" component="div">
+						{`${headerText} 사장님, 어디 계세요?!`}
+					</Typography>
+				</Toolbar>
+			</AppBar>
 
-                        <List subheader={
-                            <ListSubheader component="div">
-                                카테고리
-                            </ListSubheader>}>
+			<Drawer open={menuOpen} onClose={() => setMenuOpen(false)}>
+				<Box sx={500} role="presentation">
+					<List>
+						<ListItem button>
+							<ListItemAvatar>
+								<Avatar>
+									<LoginIcon></LoginIcon>
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText onClick={viewLogin} primary="로그인"></ListItemText>
+						</ListItem>
 
-                            {categories.map(cate => (
-                                <ListItem button sx={{ pl: 10 }}>
-                                    <ListItemText primary={cate}></ListItemText>
-                                </ListItem>
-                            ))}
-                        </List>
+						<List subheader={
+							<ListSubheader component="div">
+								카테고리
+							</ListSubheader>}>
 
-                        <ListItem button sx={{ pr: 10 }}>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <FavoriteIcon></FavoriteIcon>
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText onClick={viewFavorite} primary="즐겨찾기"></ListItemText>
-                        </ListItem>
+							{categories.map(cate => (
+								<ListItem button sx={{ pl: 10 }}>
+									<ListItemText primary={cate}></ListItemText>
+								</ListItem>
+							))}
+						</List>
 
-                        <ListItem button sx={{ pr: 10 }}>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <MyLocationIcon></MyLocationIcon>
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText onClick={viewReport} primary="제보하기"></ListItemText>
-                        </ListItem>
+						<ListItem button sx={{ pr: 10 }}>
+							<ListItemAvatar>
+								<Avatar>
+									<FavoriteIcon></FavoriteIcon>
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText onClick={viewFavorite} primary="즐겨찾기"></ListItemText>
+						</ListItem>
 
-                        <ListItem button sx={{ pr: 10 }}>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <ManageSearchIcon></ManageSearchIcon>
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText onClick={viewManage} primary="내 가게 관리하기"></ListItemText>
-                        </ListItem>
-                    </List>
-                </Box>
-            </Drawer>
+						<ListItem button sx={{ pr: 10 }}>
+							<ListItemAvatar>
+								<Avatar>
+									<MyLocationIcon></MyLocationIcon>
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText onClick={viewReport} primary="제보하기"></ListItemText>
+						</ListItem>
 
-            <div className="main-split">
-                <div className="main-split-element">
-                    <Manage />
-                </div>
+						<ListItem button sx={{ pr: 10 }}>
+							<ListItemAvatar>
+								<Avatar>
+									<ManageSearchIcon></ManageSearchIcon>
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText onClick={viewManage} primary="내 가게 관리하기"></ListItemText>
+						</ListItem>
+					</List>
+				</Box>
+			</Drawer>
 
-                <div className="main-split-element">
-                    <Map center={{ lat: 37.413294, lng: 126.79581 }} level={7} className="main-map"></Map>
-                </div>
-            </div>
-        </div>
-    );
+			<div className="main-split">
+				<div className="main-split-element">
+					<Manage />
+				</div>
+
+				<div className="main-split-element">
+					<SearchBar
+						value={searchText}
+						onChange={(text) => setsearchText(text)}
+						closeIcon={<SearchIcon />}
+						onCancelSearch={searchLocation}
+						onRequestSearch={searchLocation}
+					/>
+					<Map center={{ lat: 37.413294, lng: 126.79581 }} level={7} className="main-map"></Map>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Main;
--- frontend/src/pages/Report.js
+++ frontend/src/pages/Report.js
@@ -4,109 +4,125 @@
 import { useNavigate } from 'react-router-dom';
 import { Map } from "react-kakao-maps-sdk";
 import { FormGroup, Checkbox, FormControlLabel, Chip } from '@mui/material';
+import SearchBar from 'material-ui-search-bar';
+import SearchIcon from '@mui/icons-material/Search';
 import '../styles/Report.css'
+import { useState } from 'react';
 
 function Report() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
+	const [searchText, setsearchText] = useState("")
 
-    function viewBack() {
-        navigate('../');
-    }
-    function handleDelete() { }
-    return (
-        <div className="layout" >
+	function viewBack() {
+		navigate('../');
+	}
+	function handleDelete() { }
 
-            <div className="title"> 제보하기</div>
+	function searchLocation() {
+		if (searchText === "") { return }
+		console.log(searchText)
+		setsearchText("")
+	}
+	return (
+		<div className="layout" >
 
-
-            <div className="label">
-                *표시된 항목은 필수입력 항목입니다.
-            </div>
-
-            <div className="item">
-                <div className="info">가게 이름</div>
-                <div className="content">
-                    <input className="input" />
-                </div>
-            </div>
-
-            <div className="item">
-                <div className="info">카테고리 *</div>
-                <div className="content">
-                    <input className="input" />
-                    <div className="categories">
-                        <div className="chip" >
-                            <Chip label="붕어빵/잉어빵" color="primary" onDelete={handleDelete} />
-                        </div>
-                        <div className="chip" >
-                            <Chip label="타코야끼" color="primary" onDelete={handleDelete} />
-                        </div>
-                        <div className="chip" >
-                            <Chip label="기타" color="primary" onDelete={handleDelete} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="item">
-                <div className="info">가게 위치*</div>
-                <div className="content">
-                    (  검색창  )
-                    <div className="map-container">
-                        <Map className="map" center={{ lat: 37.413294, lng: 126.79581 }} level={7}></Map>
-                    </div>
-                    <div className="locations">
-                        <div className="chip">
-                            <Chip label="서울특별시 동대문구 서울시립대로 163" color="warning" onDelete={handleDelete} /></div>
-                        <div className="chip">
-                            <Chip label="서울특별시 마포구 ㅁㅇㄴㄻㅇㄹ" color="warning" onDelete={handleDelete} /></div>
-                        <div className="chip">
-                            <Chip label="제주특별자치도 서귀포시 ㅁㄴㄻㄴㅇ" color="warning" onDelete={handleDelete} /></div>
-
-                    </div>
-
-                </div>
-            </div>
+			<div className="title"> 제보하기</div>
 
 
-            <div className="item">
-                <div className="info">결제 수단</div>
-                <div className="content">
-                    <div className="payments">
-                        <FormGroup className="check-agree">
-                            <FormControlLabel control={<Checkbox defaultChecked />} label="현금" />
-                        </FormGroup>
-                        <FormGroup className="check-agree">
-                            <FormControlLabel control={<Checkbox defaultChecked />} label="카드" />
-                        </FormGroup>
-                        <FormGroup className="check-agree">
-                            <FormControlLabel control={<Checkbox defaultChecked />} label="계좌이체" />
-                        </FormGroup>
-                        <FormGroup className="check-agree">
-                            <FormControlLabel control={<Checkbox defaultChecked />} label="카카오페이" />
-                        </FormGroup>
+			<div className="label">
+				*표시된 항목은 필수입력 항목입니다.
+			</div>
 
-                    </div>
-                </div>
+			<div className="item">
+				<div className="info">가게 이름</div>
+				<div className="content">
+					<input className="input" />
+				</div>
+			</div>
 
-            </div>
+			<div className="item">
+				<div className="info">카테고리 *</div>
+				<div className="content">
+					<input className="input" />
+					<div className="categories">
+						<div className="chip" >
+							<Chip label="붕어빵/잉어빵" color="primary" onDelete={handleDelete} />
+						</div>
+						<div className="chip" >
+							<Chip label="타코야끼" color="primary" onDelete={handleDelete} />
+						</div>
+						<div className="chip" >
+							<Chip label="기타" color="primary" onDelete={handleDelete} />
+						</div>
+					</div>
+				</div>
+			</div>
 
-            <div className="item">
-                <div className="info">가게 사진</div>
-                <div className="content">
-                    <div className="image-uploader">
-                        사진 업로더
+			<div className="item">
+				<div className="info">가게 위치*</div>
+				<div className="content">
+					<div className="map-container">
+						<SearchBar
+							value={searchText}
+							onChange={(text) => setsearchText(text)}
+							closeIcon={<SearchIcon />}
+							onCancelSearch={searchLocation}
+							onRequestSearch={searchLocation}
+						/>
+						<Map className="map" center={{ lat: 37.413294, lng: 126.79581 }} level={7}></Map>
+					</div>
+					<div className="locations">
+						<div className="chip">
+							<Chip label="서울특별시 동대문구 서울시립대로 163" color="warning" onDelete={handleDelete} /></div>
+						<div className="chip">
+							<Chip label="서울특별시 마포구 ㅁㅇㄴㄻㅇㄹ" color="warning" onDelete={handleDelete} /></div>
+						<div className="chip">
+							<Chip label="제주특별자치도 서귀포시 ㅁㄴㄻㄴㅇ" color="warning" onDelete={handleDelete} /></div>
 
-                    </div>
-                </div>
-            </div>
+					</div>
 
-            <div className="item">
-                <div className="register-button"> 등록하기 </div>
-            </div>
+				</div>
+			</div>
 
-        </div>
-    );
+
+			<div className="item">
+				<div className="info">결제 수단</div>
+				<div className="content">
+					<div className="payments">
+						<FormGroup className="check-agree">
+							<FormControlLabel control={<Checkbox defaultChecked />} label="현금" />
+						</FormGroup>
+						<FormGroup className="check-agree">
+							<FormControlLabel control={<Checkbox defaultChecked />} label="카드" />
+						</FormGroup>
+						<FormGroup className="check-agree">
+							<FormControlLabel control={<Checkbox defaultChecked />} label="계좌이체" />
+						</FormGroup>
+						<FormGroup className="check-agree">
+							<FormControlLabel control={<Checkbox defaultChecked />} label="카카오페이" />
+						</FormGroup>
+
+					</div>
+				</div>
+
+			</div>
+
+			<div className="item">
+				<div className="info">가게 사진</div>
+				<div className="content">
+					<div className="image-uploader">
+						사진 업로더
+
+					</div>
+				</div>
+			</div>
+
+			<div className="item">
+				<div className="register-button"> 등록하기 </div>
+			</div>
+
+		</div>
+	);
 }
 
 export default Report;
