5df56f83ee2b3a5a95331ef656a7a77c85900700
moran991231 <moran991231@naver.com>
2021-12-07 18:32:42 +0900

    remove cookieprovider

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -13,16 +13,11 @@ import "../styles/Main.css"
 import Login from "./Login"
 import SignUp from "./SignUp"
 import Manage from "./Manage"
-import { CookiesProvider } from 'react-cookie';
 import { getUserCookie } from '../shared/cookie';
 import { logout } from '../shared/BackendRequests';
 import { categories } from '../shared/constantLists'
 
 function Main(props) {
-    // const categories = [
-    //     "전체", "붕어빵/잉어빵", "타코야끼", "풀빵", "호떡", "군고구마", "꼬치", "분식", "기타"
-    // ];
-
     const navigate = useNavigate();
     const [headerText, setHeaderText] = useState("군것질");
     const [menuOpen, setMenuOpen] = useState(false);
@@ -49,91 +44,89 @@ function Main(props) {
 
 
     return (
-        <CookiesProvider>
-            <div className="main-layout">
-                <AppBar position="static" color="transparent" elevation={0}>
-                    <Toolbar>
-                        <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}
-                            onClick={() => setMenuOpen(true)}>
-                            <MenuIcon></MenuIcon>
-                        </IconButton>
+        <div className="main-layout">
+            <AppBar position="static" color="transparent" elevation={0}>
+                <Toolbar>
+                    <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}
+                        onClick={() => setMenuOpen(true)}>
+                        <MenuIcon></MenuIcon>
+                    </IconButton>
 
-                        <Typography className="main-header-text" variant="h5" component="div">
-                            {`${headerText} 사장님, 어디 계세요?!`}
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+                    <Typography className="main-header-text" variant="h5" component="div">
+                        {`${headerText} 사장님, 어디 계세요?!`}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
 
-                <Drawer open={menuOpen} onClose={() => setMenuOpen(false)}>
-                    <Box sx={500} role="presentation">
-                        <List>
-                            <ListItem button>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <LoginIcon></LoginIcon>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText onClick={viewLogin} primary="로그인"></ListItemText>
-                            </ListItem>
+            <Drawer open={menuOpen} onClose={() => setMenuOpen(false)}>
+                <Box sx={500} role="presentation">
+                    <List>
+                        <ListItem button>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <LoginIcon></LoginIcon>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText onClick={viewLogin} primary="로그인"></ListItemText>
+                        </ListItem>
 
-                            <List subheader={
-                                <ListSubheader component="div">
-                                    카테고리
-                                </ListSubheader>}>
+                        <List subheader={
+                            <ListSubheader component="div">
+                                카테고리
+                            </ListSubheader>}>
 
-                                {categories.map(cate => (
-                                    <ListItem button sx={{ pl: 10 }}>
-                                        <ListItemText primary={cate}></ListItemText>
-                                    </ListItem>
-                                ))}
-                            </List>
-
-                            <ListItem button sx={{ pr: 10 }}>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <FavoriteIcon></FavoriteIcon>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText onClick={viewFavorite} primary="즐겨찾기"></ListItemText>
-                            </ListItem>
-
-                            <ListItem button sx={{ pr: 10 }}>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <MyLocationIcon></MyLocationIcon>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText onClick={viewReport} primary="제보하기"></ListItemText>
-                            </ListItem>
-
-                            <ListItem button sx={{ pr: 10 }}>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <ManageSearchIcon></ManageSearchIcon>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText onClick={viewManage} primary="내 가게 관리하기"></ListItemText>
-                            </ListItem>
+                            {categories.map(cate => (
+                                <ListItem button sx={{ pl: 10 }}>
+                                    <ListItemText primary={cate}></ListItemText>
+                                </ListItem>
+                            ))}
                         </List>
-                    </Box>
-                </Drawer>
 
-                <div className="main-split">
-                    <div className="main-split-element">
-                        {/* <Manage reportManage={0} /> */}
-                        {/* <Login /> */}
-                        <SignUp />
-                    </div>
+                        <ListItem button sx={{ pr: 10 }}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <FavoriteIcon></FavoriteIcon>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText onClick={viewFavorite} primary="즐겨찾기"></ListItemText>
+                        </ListItem>
 
-                    <div className="main-split-element">
-                        {/* <Manage reportManage={1} /> */}
-                        <Login />
+                        <ListItem button sx={{ pr: 10 }}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <MyLocationIcon></MyLocationIcon>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText onClick={viewReport} primary="제보하기"></ListItemText>
+                        </ListItem>
 
-                        {/* <Map center={{ lat: 37.413294, lng: 126.79581 }} level={7} className="main-map"></Map> */}
-                    </div>
+                        <ListItem button sx={{ pr: 10 }}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <ManageSearchIcon></ManageSearchIcon>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText onClick={viewManage} primary="내 가게 관리하기"></ListItemText>
+                        </ListItem>
+                    </List>
+                </Box>
+            </Drawer>
+
+            <div className="main-split">
+                <div className="main-split-element">
+                    <Manage reportManage={0} />
+                    {/* <Login /> */}
+                    {/* <Manage /> */}
+                </div>
+
+                <div className="main-split-element">
+                    {/* <Manage reportManage={1} /> */}
+                    <Login />
+
+                    {/* <Map center={{ lat: 37.413294, lng: 126.79581 }} level={7} className="main-map"></Map> */}
                 </div>
             </div>
-        </CookiesProvider>
+        </div>
     );
 }
 
